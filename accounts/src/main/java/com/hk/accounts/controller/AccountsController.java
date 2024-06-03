e0de6434309a0d6ab589c18e1f616a9b438ae6c8
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-03 13:25:03 +0530

    [ACCOUNTS_SERVICE] add validation to dtos

--- accounts/src/main/java/com/hk/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/hk/accounts/controller/AccountsController.java
@@ -4,21 +4,25 @@ import com.hk.accounts.constants.AccountsConstants;
 import com.hk.accounts.dto.CustomerDto;
 import com.hk.accounts.dto.ResponseDto;
 import com.hk.accounts.service.AccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private final AccountsService accountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -26,14 +30,14 @@ public class AccountsController {
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits") String mobileNumber) {
         CustomerDto customerDto = accountsService.fetchAccount(mobileNumber);
 
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
 
         if(isUpdated) {
@@ -45,7 +49,8 @@ public class AccountsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})",
+            message = "AccountNumber must be 10 digits") String mobileNumber) {
         boolean isDeleted = accountsService.deleteAccount(mobileNumber);
         if(isDeleted) {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
--- accounts/src/main/java/com/hk/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/hk/accounts/dto/AccountsDto.java
@@ -1,13 +1,19 @@
 package com.hk.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
+    @NotEmpty(message = "AccountNumber cannot be null or empty")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 }
