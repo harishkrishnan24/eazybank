938ac99c9e83f6f11d420bb2eefb73891f776871
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-03 15:35:12 +0530

    [CARDS_SERVICE] CRUD for cards

---
+++ cards/src/main/java/com/hk/cards/service/ICardsService.java
@@ -0,0 +1,14 @@
+package com.hk.cards.service;
+
+import com.hk.cards.dto.CardsDto;
+
+public interface ICardsService {
+
+    void createCard(String mobileNumber);
+
+    CardsDto fetchCard(String mobileNumber);
+
+    boolean updateCard(CardsDto cardsDto);
+
+    boolean deleteCard(String mobileNumber);
+}
